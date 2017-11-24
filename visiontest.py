f028b44e78707a4323f226a25bd212aaa4ce3200
OriKeidar <oriko122@gmail.com>
2017-11-24 20:17:02 +0200

    expirimental rect detection by area added

--- visiontest.py
+++ visiontest.py
@@ -31,13 +31,16 @@ while True:
         # draw a green rectangle to visualize the bounding rect
         cv2.rectangle(res, (x, y), (x+w, y+h), (0, 255, 0), 2)
     #draw a center plus
-    if len(contours) == 2:
-        x1,y1,w1,h1= cv2.boundingRect(contours[0])
-        x2,y2,w2,h2= cv2.boundingRect(contours[1])
-        pt1 = ((x1+w1/2+x2+w2/2)/2)
-        pt2 = ((y1+h1/2+y2+h2/2)/2)
-        cv2.line(res,(pt1 + 7,pt2),(pt1 - 7, pt2),(0,0,255),2)
-        cv2.line(res,(pt1, pt2+7),(pt1,pt2-7),(0,0,255),2)
+    #checks if rectangles area equal, if so it draws a center plus
+    for c in contours:
+        for sc in contours:
+            x1,y1,w1,h1= cv2.boundingRect(c)
+            x2,y2,w2,h2= cv2.boundingRect(sc)
+            if w1*h1 == w2*h2:
+                pt1 = ((x1+w1/2+x2+w2/2)/2)
+                pt2 = ((y1+h1/2+y2+h2/2)/2)
+                cv2.line(res,(pt1 + 7,pt2),(pt1 - 7, pt2),(0,0,255),2)
+                cv2.line(res,(pt1, pt2+7),(pt1,pt2-7),(0,0,255),2)
     #display
     cv2.imshow("source",frame)
     cv2.imshow("mask",mask)
