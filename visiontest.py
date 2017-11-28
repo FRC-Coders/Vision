4f4044d158147824a4ea979c67f0423d47747648
Cyben <benshaver8@gmail.com>
2017-11-28 20:14:43 +0200

    added distance calculations

--- visiontest.py
+++ visiontest.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import Tkinter as tk
+import TheJokerLib_Vision as jk
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -12,32 +13,43 @@ while True:
     #bluring
     blur = cv2.blur(hsv,(5,5))
     #define color range
-    lower_green = np.array([0,0,100])
-    upper_green = np.array([255,150,255])
+    lower_green = np.array([44,0,252])
+    upper_green = np.array([91,47,255])
     
     #create mask
     mask = cv2.inRange(blur, lower_green,upper_green)
+
+    mask = jk.dil_ero(mask,(300,300),3)
     #create res
     res = cv2.bitwise_and(frame,frame,mask=mask)
-    #create threshold
-    ret,thresh = cv2.threshold(mask,127,255,cv2.THRESH_BINARY)
+
+    
     
     #find contours
-    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(res, contours, -1, (45,255,30), 3)
+    _,contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+
     for c in contours:
         # get the bounding rect
         x, y, w, h = cv2.boundingRect(c)
         # draw a green rectangle to visualize the bounding rect
-        cv2.rectangle(res, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+    cnts = []
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)
+        if (2<=float(h)/w<=3) and h*w>1000:
+            cv2.rectangle(res, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cnts.append(c)
+    cv2.drawContours(res, cnts, -1, (45,255,30), 3)
     #draw a center plus
-    if len(contours) == 2:
-        x1,y1,w1,h1= cv2.boundingRect(contours[0])
-        x2,y2,w2,h2= cv2.boundingRect(contours[1])
-        pt1 = ((x1+w1/2+x2+w2/2)/2)
-        pt2 = ((y1+h1/2+y2+h2/2)/2)
-        cv2.line(res,(pt1 + 7,pt2),(pt1 - 7, pt2),(0,0,255),2)
-        cv2.line(res,(pt1, pt2+7),(pt1,pt2-7),(0,0,255),2)
+    if len(cnts) > 1 :
+        x1,y1,w1,h1= cv2.boundingRect(cnts[0])
+        x2,y2,w2,h2= cv2.boundingRect(cnts[1])
+     #   pt1 = ((x1+w1/2+x2+w2/2)/2)
+      #  pt2 = ((y1+h1/2+y2+h2/2)/2)
+       # cv2.line(res,(pt1 + 7,pt2),(pt1 - 7, pt2),(0,0,255),2)
+        #cv2.line(res,(pt1, pt2+7),(pt1,pt2-7),(0,0,255),2)
+        cv2.putText(res,str((((2.0*714)/((w1+w2)/2.0))*2.54)),(x1,y1-20),1,5,(255,255,255))
     #display
     cv2.imshow("source",frame)
     cv2.imshow("mask",mask)
